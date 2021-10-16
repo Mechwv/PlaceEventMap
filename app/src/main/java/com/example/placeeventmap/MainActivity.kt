1fb0da243517e2f873ab3cced68021e0a180eec6
Mechwv <mechwv@gmail.com>
2021-10-16 23:38:14 +0300

    bottom nav test

--- app/src/main/java/com/example/placeeventmap/MainActivity.kt
+++ app/src/main/java/com/example/placeeventmap/MainActivity.kt
@@ -2,6 +2,13 @@ package com.example.placeeventmap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.placeeventmap.databinding.ActivityMainBinding
+import com.example.placeeventmap.databinding.PlacesListFragmentBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(findViewById<BottomNavigationView>(R.id.bottomNav), navController)
     }
 }
