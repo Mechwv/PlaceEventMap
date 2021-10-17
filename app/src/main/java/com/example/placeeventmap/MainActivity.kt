1fcb184be88d759b5d8c61cfb6d183cba7e5ed3e
Mechwv <mechwv@gmail.com>
2021-10-17 22:31:47 +0300

    bug fixes + gps udpate

--- app/src/main/java/com/example/placeeventmap/MainActivity.kt
+++ app/src/main/java/com/example/placeeventmap/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.placeeventmap.databinding.ActivityMainBinding
 import com.example.placeeventmap.databinding.PlacesListFragmentBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MapKitFactory.setApiKey(BuildConfig.YANDEX_KEY)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
